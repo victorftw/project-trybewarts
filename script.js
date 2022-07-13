a893e5d9f36904381f189623e41995ad6830876c
Victor Hugo <victor.hiraiftw@outlook.com>
2022-07-13 17:59:44 -0300

    requisito 18 concluido

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const botaoHeader = document.getElementById('botao-header');
-const inputPassword = document.getElementById('senha-header');
-const inputEmail = document.getElementById('email-header');
+const botaoHeader = document.getElementById("botao-header");
+const inputPassword = document.getElementById("senha-header");
+const inputEmail = document.getElementById("email-header");
 
-botaoHeader.addEventListener('click', () => {
+botaoHeader.addEventListener("click", () => {
   if (
-    inputPassword.value === '123456' && inputEmail.value === 'tryber@teste.com'
+    inputPassword.value === "123456" &&
+    inputEmail.value === "tryber@teste.com"
   ) {
-    alert('Olá, Tryber!');
+    alert("Olá, Tryber!");
   } else {
-    alert('Email ou senha inválidos.');
+    alert("Email ou senha inválidos.");
   }
 });
+
+const checkbox = document.querySelector("#agreement");
+const submitBtn = document.querySelector("#submit-btn");
+
+checkbox.addEventListener("click", (e) => {
+  if (checkbox.checked === true) {
+    submitBtn.disabled = false;
+  }
+  if (checkbox.checked === false) {
+    submitBtn.disabled = true;
+  }
+});
+
+window.onload = () => {
+  submitBtn.disabled = true;
+};
